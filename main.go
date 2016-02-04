6bc1b9aa306cd98d4b40dac46d9734b70a4b8ade
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-02-04 13:46:35 +0100

    initial cli

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+)
+
 func main() {
 
+	app := cli.NewApp()
+	app.Name = "Identity server"
+	app.Version = "0.1-Dev"
+
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	var debugLogging bool
+	var bindAddress string
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:        "debug, d",
+			Usage:       "Enable debug logging",
+			Destination: &debugLogging,
+		},
+		cli.StringFlag{
+			Name:        "bind, b",
+			Usage:       "Bind address",
+			Value:       ":8080",
+			Destination: &bindAddress,
+		},
+	}
+	app.Before = func(c *cli.Context) error {
+		if debugLogging {
+			log.SetLevel(log.DebugLevel)
+			log.Debug("Debug logging enabled")
+			log.Debug(app.Name, "-", app.Version)
+		}
+		return nil
+	}
+
+	app.Action = func(c *cli.Context) {
+
+		//r, err := server.GetRouter()
+
+		//if err != nil {
+		//	log.Error(err)
+		//	os.Exit(1)
+		//}
+
+		//r.Run(bindAddress)
+	}
+
+	app.Run(os.Args)
 }
