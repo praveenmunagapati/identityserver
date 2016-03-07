59b98d0ed5b5f2618ea2a12146a0d0731ff167f6
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-03-07 12:31:05 +0100

    Angular company and organization structure in the toplevel app

---
+++ components/company/controller.js
@@ -0,0 +1,14 @@
+angular.module("itsyouonlineApp").controller("CompanyController",CompanyController);
+
+
+CompanyController.$inject = ['$location'];
+
+function CompanyController($location) {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+
+    }
+}
---
+++ components/organization/controller.js
@@ -0,0 +1,14 @@
+angular.module("itsyouonlineApp").controller("OrganizationController",OrganizationController);
+
+
+OrganizationController.$inject = ['$location'];
+
+function OrganizationController($location) {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+
+    }
+}
