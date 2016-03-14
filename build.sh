eecfd4db2e94c033b3ea41cb6e7220c5d64d31ec
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-03-14 19:59:26 +0100

    Set the correct docker repository

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t itsyouonlinebuilder .
 docker run --rm -v "$PWD":/go/src/github.com/itsyouonline/identityserver --entrypoint go  itsyouonlinebuilder build -ldflags '-s' -v -o dist/identityserver
-docker build -t itsyouonline:latest -f DockerfileMinimal .
+docker build -t itsyouonline/identityserver:latest -f DockerfileMinimal .
