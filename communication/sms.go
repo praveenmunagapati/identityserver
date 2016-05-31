0416f675f33dc31bd617cb6e7a80637ee84cf063
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-05-31 17:35:53 +0200

    Log errors when sending an sms request

--- communication/sms.go
+++ communication/sms.go
@@ -7,7 +7,7 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 )
 
 //SMSService is an SMS communication channel using Twilio
@@ -29,18 +29,20 @@ func (s *SMSService) Send(phonenumber string, message string) (err error) {
 
 	req, err := http.NewRequest("POST", "https://api.twilio.com/2010-04-01/Accounts/"+s.AccountSID+"/Messages.json", strings.NewReader(data.Encode()))
 	if err != nil {
+		log.Error("Error creating sms request: ", err)
 		return
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.SetBasicAuth(s.AccountSID, s.AuthToken)
 	resp, err := client.Do(req)
 	if err != nil {
+		log.Error("Error sending sms via Twilio: ", err)
 		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusAccepted {
-		logrus.Error("Problem when sending sms via Twilio: ", resp.StatusCode, "\n", body)
+		log.Error("Problem when sending sms via Twilio: ", resp.StatusCode, "\n", body)
 		err = errors.New("Error sending sms")
 	}
 	return
