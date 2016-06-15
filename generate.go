3849ccae989350eae6a6943f0dc80f8a18984e0a
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-06-15 18:39:52 +0200

    Add the go client generation to go:generate

--- generate.go
+++ generate.go
@@ -3,10 +3,7 @@ package main
 //This file contains the go:generate commands
 
 //go-raml https://github.com/Jumpscale/go-raml server code generation from the RAML specification
-//go:generate go-raml server --ramlfile specifications/api/users.raml --dir identityservice/user --package user --no-main
-//go:generate go-raml server --ramlfile specifications/api/companies.raml --dir identityservice/company --package company --no-main
-//go:generate go-raml server --ramlfile specifications/api/organizations.raml --dir identityservice/organization --package organization --no-main
-//go:generate go-raml server --ramlfile specifications/api/contracts.raml --dir identityservice/contract --package contract --no-main
-//go:generate go-raml server --ramlfile specifications/api/userorganizations.raml --dir identityservice/userorganization --package userorganization --no-main
+//TODO: fix serverside code generation with go-raml
+//go:generate go-raml client -l go --dir clients/go/itsyouonline --ramlfile specifications/api/itsyouonline.raml
 
 //go:generate go-bindata -pkg specifications -prefix specifications/api -o specifications/packaged.go specifications/api/...
