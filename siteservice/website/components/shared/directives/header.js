f930dcd61c5595bd5f33a5822eb46b884c1a567f
Lucas Vanhalst <lucas@mobicage.com>
2016-06-17 09:23:02 +0200

    Cookie warning

--- siteservice/website/components/shared/directives/header.js
+++ siteservice/website/components/shared/directives/header.js
@@ -8,6 +8,13 @@
                 templateUrl: 'components/shared/directives/header.html',
                 link: function (scope, element, attr) {
                     scope.header_login = attr.register !== undefined;
+                    scope.showCookieWarning = !localStorage.getItem('cookiewarning-dismissed');
+                    scope.hideCookieWarning  = hideCookieWarning;
+
+                    function hideCookieWarning(){
+                        localStorage.setItem('cookiewarning-dismissed', true);
+                        scope.showCookieWarning = false;
+                    }
                 }
             };
         });
