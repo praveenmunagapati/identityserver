abdafdf12280c1ad7bc76514c7cd5d42fe15bcfb
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-07-07 17:00:13 +0200

    Build 0.9-beta-1

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 
 docker build -t itsyouonlinebuilder .
 docker run --rm -v "$PWD":/go/src/github.com/itsyouonline/identityserver --entrypoint go  itsyouonlinebuilder build -ldflags '-s' -v -o dist/identityserver
-docker build -t itsyouonline/identityserver:latest -f DockerfileMinimal .
+docker build -t itsyouonline/identityserver:0.9-beta-1 -f DockerfileMinimal .
