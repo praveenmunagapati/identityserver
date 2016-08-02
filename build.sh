1b77413756a5a2e21f99421a5da0e5ca300be29e
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-08-02 15:51:16 +0200

    Bump version

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 
 docker build -t itsyouonlinebuilder .
-docker run --rm -v "$PWD":/go/src/github.com/itsyouonline/identityserver --entrypoint go  itsyouonlinebuilder build -ldflags '-s' -v -o dist/identityserver
-docker build -t itsyouonline/identityserver:0.9-beta-2 -f DockerfileMinimal .
+docker run --rm -v "$PWD":/go/src/github.com/itsyouonline/identityserver --entrypoint go itsyouonlinebuilder build -ldflags '-s' -v -o dist/identityserver
+docker build -t itsyouonline/identityserver:0.9-beta-3 -f DockerfileMinimal .
