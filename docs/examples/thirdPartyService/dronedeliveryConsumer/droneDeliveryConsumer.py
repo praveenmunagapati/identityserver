079d33920a24859c07136171b22c054e5332a446
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2016-08-02 15:33:57 +0200

    Complete example with documentation (#227)
    * Complete example with documentation
    
    * small changes in documentation

--- docs/examples/thirdPartyService/dronedeliveryConsumer/droneDeliveryConsumer.py
+++ docs/examples/thirdPartyService/dronedeliveryConsumer/droneDeliveryConsumer.py
@@ -1,7 +1,28 @@
-__author__ = 'Dylan'
-from dronedeliveryConsumer.droneDeliveryClient.client import Client
+from docs.examples.thirdPartyService.dronedeliveryConsumer.droneDeliveryClient.client import Client
+from clients.python import itsyouonline
+import requests
 
-client = Client()
-client.url = "http://127.0.0.1:5000"
+itsyouonline_client = itsyouonline.Client()
+itsyouonline_client_applicationid = 'FILL_IN_APPLICATIONID'
+itsyouonline_client_secret = 'FILL_IN_CLIENTSECRET'
+itsyouonline_client.oauth.LoginViaClientCredentials(client_id=itsyouonline_client_applicationid,
+                                                    client_secret=itsyouonline_client_secret)
 
-print(client.deliveries_get().json())
+
+def getjwt():
+    uri = "https://itsyou.online/v1/oauth/jwt?aud=dronedelivery&scope="
+    r = requests.post(uri, headers=itsyouonline_client.oauth.session.headers)
+    if r.status_code != 200:
+        return r.text
+    else:
+        return r.status_code
+
+
+dronedelivery_consumer_client = Client()
+dronedelivery_consumer_client.url = "http://127.0.0.1:5000"
+jwt = getjwt()
+dronedelivery_consumer_client.set_auth_header("bearer %s" % jwt)
+r = dronedelivery_consumer_client.deliveries_get()
+
+print(r)
+print(r.json())
--- docs/examples/thirdPartyService/dronedeliveryService/app.py
+++ docs/examples/thirdPartyService/dronedeliveryService/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, send_from_directory, send_file
+
 import wtforms_json
 from deliveries import deliveries_api
 from drones import drones_api
-
 
 app = Flask(__name__)
 
@@ -13,7 +13,6 @@ app.register_blueprint(deliveries_api)
 app.register_blueprint(drones_api)
 
 
-
 @app.route('/apidocs/<path:path>')
 def send_js(path):
     return send_from_directory('apidocs', path)
@@ -22,6 +21,7 @@ def send_js(path):
 @app.route('/', methods=['GET'])
 def home():
     return send_file('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
