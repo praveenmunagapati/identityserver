dd56da69999a8ef738052c4bd117880922101ef0
Rob Van Mieghem <rob@incubaid.com>
2016-08-08 14:39:23 +0200

    generate the website when building

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
-FROM golang:1.6
+FROM golang:1.6.3
 MAINTAINER itsyou.online
+
+RUN git clone https://github.com/jteeuwen/go-bindata.git $GOPATH/src/github.com/jteeuwen/go-bindata
+WORKDIR $GOPATH/src/github.com/jteeuwen/go-bindata
+RUN git checkout a0ff2567cfb70903282db057e799fd826784d41d
+RUN go get github.com/jteeuwen/go-bindata/...
 
 ENV CGO_ENABLED 0
 WORKDIR /go/src/github.com/itsyouonline/identityserver
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ VERSION="$(git describe)"
 echo "Building version $VERSION"
 
 docker build -t itsyouonlinebuilder .
-docker run --rm -v "$PWD":/go/src/github.com/itsyouonline/identityserver --entrypoint go itsyouonlinebuilder build -ldflags '-s' -v -o dist/identityserver
+docker run --rm -v "$PWD":/go/src/github.com/itsyouonline/identityserver --entrypoint sh itsyouonlinebuilder -c "cd siteservice/website && go generate && cd ../.. && go build -ldflags '-s' -v -o dist/identityserver"
 docker build -t itsyouonline/identityserver:"$VERSION" -f DockerfileMinimal .
 
 docker push itsyouonline/identityserver:"$VERSION"
