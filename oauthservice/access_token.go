44f6b6556aef3f08017964c4ba054b131559d24a
Rob Van Mieghem <rob@incubaid.com>
2016-09-15 11:31:43 +0200

    Issue #251 Accept 'authorization_code' grant type in the access token handler

--- oauthservice/access_token.go
+++ oauthservice/access_token.go
@@ -71,6 +71,11 @@ func (service *Service) AccessTokenHandler(w http.ResponseWriter, r *http.Reques
 	clientSecret := r.FormValue("client_secret")
 	clientID := r.FormValue("client_id")
 
+	//Also accept some alternatives
+	if grantType == "authorization_code" {
+		grantType = ""
+	}
+
 	if clientSecret == "" || clientID == "" || (grantType == "" && code == "") {
 		log.Debug("Required parameter missing in the request")
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
@@ -85,6 +90,7 @@ func (service *Service) AccessTokenHandler(w http.ResponseWriter, r *http.Reques
 		if grantType == ClientCredentialsGrantCodeType {
 			at, httpStatusCode = clientCredentialsTokenHandler(clientID, clientSecret, mgr, r)
 		} else {
+			log.Debug("Invalid grant_type")
 			httpStatusCode = http.StatusBadRequest
 		}
 	} else {
