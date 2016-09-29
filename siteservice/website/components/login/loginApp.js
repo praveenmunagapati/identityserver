a7d2c4d8cb1d950a8052f0617d146ded09664503
Lee Smet <lee.smet@hotmail.com>
2016-09-29 15:33:34 +0200

    Improved #240: Cobranding - merge login and register screen (#260)

--- siteservice/website/components/login/loginApp.js
+++ siteservice/website/components/login/loginApp.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('loginApp', ['ngMaterial', 'ngMessages', 'ngRoute', 'ngSanitize',
+    angular.module('loginApp', ['ngMaterial', 'ngMessages', 'ngRoute', 'ngSanitize', 'monospaced.qrcode', 'itsyouonline.shared',
         'itsyouonline.header', 'itsyouonline.footer', 'itsyouonline.user', 'itsyouonline.validation'])
         .config(['$mdThemingProvider', themingConfig])
         .config(['$httpProvider', httpConfig])
@@ -9,24 +9,6 @@
 
     function themingConfig($mdThemingProvider) {
         $mdThemingProvider.definePalette('blueish', {
-            '50': '#f7fbfd',
-            '100': '#badeed',
-            '200': '#8ec8e2',
-            '300': '#55add3',
-            '400': '#3ca1cd',
-            '500': '#3091bb',
-            '600': '#2a7ea3',
-            '700': '#236b8a',
-            '800': '#1d5872',
-            '900': '#17455a',
-            'A100': '#f7fbfd',
-            'A200': '#badeed',
-            'A400': '#3ca1cd',
-            'A700': '#236b8a',
-            'contrastDefaultColor': 'light',
-            'contrastDarkColors': '50 100 200 300 400 A100 A200 A400'
-        });
-        $mdThemingProvider.definePalette('centrilogic', {
             '50': '#f7fbfd',
             '100': '#badeed',
             '200': '#8ec8e2',
@@ -46,7 +28,7 @@
         });
         $mdThemingProvider
             .theme('default')
-            .primaryPalette('centrilogic');
+            .primaryPalette('blueish');
     }
 
     function httpConfig($httpProvider) {
--- siteservice/website/components/login/loginController.js
+++ siteservice/website/components/login/loginController.js
@@ -1,15 +1,37 @@
 (function () {
     'use strict';
     angular.module('loginApp')
-        .controller('loginController', ['$http', '$window', '$scope', '$rootScope', 'LoginService', loginController]);
+        .controller('loginController', ['$http', '$window', '$scope', '$rootScope', '$mdUtil', 'configService', 'LoginService', loginController]);
 
-    function loginController($http, $window, $scope, $rootScope, LoginService) {
+    function loginController($http, $window, $scope, $rootScope, $mdUtil, configService, LoginService) {
         var vm = this;
+        configService.getConfig(function (config) {
+            vm.totpsecret = config.totpsecret;
+        });
         vm.submit = submit;
+        vm.register = register;
         vm.clearValidation = clearValidation;
+        vm.validateUsername = validateUsername;
+        vm.registerUser = registerUser;
+        vm.resetValidation = resetValidation;
+        vm.loginInfoValid = loginInfoValid;
+        vm.basicInfoValid = basicInfoValid;
+        vm.signupInfoValid = signupInfoValid;
+        vm.moveOn = moveOn;
         vm.externalSite = URI($window.location.href).search(true).client_id;
         $rootScope.registrationUrl = '/register' + $window.location.search;
         vm.logo = "";
+        vm.twoFAMethod = 'sms';
+        vm.validateUsername = $mdUtil.debounce(function () {
+            $scope.loginform.registerlogin.$setValidity("duplicate_username", true);
+            $scope.loginform.registerlogin.$setValidity("invalid_username_format", true);
+            if ($scope.loginform.registerlogin.$valid) {
+                validateUsername(vm.registerlogin)
+                    .then(function (response) {
+                        $scope.loginform.registerlogin.$setValidity(response.data.error, response.data.valid);
+                    });
+            }
+        }, 500, true);
 
         activate();
 
@@ -58,14 +80,122 @@
                 },
                 function (response) {
                     if (response.status === 422) {
-                        $scope.loginform.password.$setValidity("invalidcredentials", false);
+                        $scope.loginform.registerpassword.$setValidity("invalidcredentials", false);
                     }
                 }
             );
         }
 
+        function register() {
+          var redirectparams = $window.location.search.replace('?', '');
+              registerUser(vm.twoFAMethod, vm.registerlogin, vm.registeremail, vm.registerpassword, vm.totpcode, vm.sms, redirectparams)
+              .then(function (response) {
+                  var url = response.data.redirecturl;
+                  if (url === '/') {
+                      $cookies.remove('registrationdetails');
+                  }
+                  $window.location.href = url;
+              }, function (response) {
+                  switch (response.status) {
+                      case 422:
+                          var err = response.data.error;
+                          switch (err) {
+                              case 'invalid_phonenumber':
+                                  $scope.loginform.phonenumber.$setValidity(err, false);
+                                  break;
+                              case 'invalid_totpcode':
+                                  $scope.loginform.totpcode.$setValidity(err, false);
+                                  break;
+                              case 'invalid_password':
+                                  vm.registration2fa = false;
+                                  $scope.loginform.registerpassword.$setValidity(err, false);
+                                  break;
+                              case 'invalid_username_format':
+                                  vm.registration2fa = false;
+                                  $scope.loginform.registerlogin.$setValidity(err, false);
+                                  break;
+                              default:
+                                  console.error('Unconfigured error:', response.data.error);
+                          }
+                          break;
+                      case 409:
+                          vm.registration2fa = false;
+                          $scope.loginform.registerlogin.$setValidity('duplicate_username', false);
+                          break;
+                  }
+              });
+        }
+
         function clearValidation() {
             $scope.loginform.password.$setValidity("invalidcredentials", true);
+        }
+
+        function validateUsername(username) {
+            var options = {
+                params: {
+                    username: username
+                }
+            };
+            return $http.get('/validateusername', options);
+        }
+
+        function registerUser(twoFAMethod, login, email, password, totpcode, sms, redirectparams) {
+            var url = '/register';
+            var data = {
+                twofamethod: twoFAMethod,
+                login: login.trim(),
+                email: email.trim(),
+                password: password,
+                totpcode: totpcode,
+                phonenumber: sms,
+                redirectparams: redirectparams
+            };
+            return $http.post(url, data);
+        }
+
+        function resetValidation(prop) {
+            switch (prop) {
+                case 'phonenumber':
+                    $scope.loginform[prop].$setValidity("invalid_phonenumber", true);
+                    break;
+                case 'totpcode':
+                    $scope.loginform[prop].$setValidity("invalid_totpcode", true);
+                    break;
+                case 'twoFAMethod':
+                    $scope.loginform.phonenumber.$setValidity("invalid_phonenumber", true);
+                    $scope.loginform.phonenumber.$setValidity("pattern", true);
+                    $scope.loginform.totpcode.$setValidity("totpcode", true);
+                    break;
+            }
+        }
+
+        function loginInfoValid() {
+            return $scope.loginform.login
+                && $scope.loginform.login.$valid
+                && $scope.loginform.password.$valid;
+        }
+
+        function basicInfoValid() {
+            return $scope.loginform.registerlogin
+                && $scope.loginform.registerlogin.$valid
+                && $scope.loginform.registeremail.$valid
+                && $scope.loginform.registerpassword.$valid
+                && $scope.loginform.passwordvalidation.$valid;
+        }
+
+        function signupInfoValid() {
+            switch (vm.twoFAMethod) {
+                case 'sms':
+                    return basicInfoValid() && $scope.loginform.phonenumber.$valid;
+                    break;
+                case 'totp':
+                    return basicInfoValid() && $scope.loginform.totpcode.$valid;
+                    break;
+            }
+        }
+
+        function moveOn() {
+            vm.registration2fa = true;
         }
     }
 })();
