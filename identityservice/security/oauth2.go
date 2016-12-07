232b59e0f9570b8f128679d9bdfdf82818d4fce1
Rob Van Mieghem <rob@incubaid.com>
2016-12-07 21:52:38 +0100

    Decently parse the parentscopes when creating a child jwt

--- identityservice/security/oauth2.go
+++ identityservice/security/oauth2.go
@@ -4,8 +4,6 @@ import (
 	"crypto/ecdsa"
 	"net/http"
 	"strings"
-
-	"github.com/dgrijalva/jwt-go"
 )
 
 // OAuth2Middleware defines the common oauth2 functionality
@@ -31,20 +29,4 @@ func (om *OAuth2Middleware) GetAccessToken(r *http.Request) string {
 	}
 	accessToken := strings.TrimSpace(strings.TrimPrefix(authorizationHeader, "token"))
 	return accessToken
-}
-
-// GetScopestringFromJWT turns the scopes from a jwt in to a commaseperated scopestring
-func GetScopestringFromJWT(token *jwt.Token) (scopestring string) {
-	if token == nil {
-		return
-	}
-	//Ignore the errors for now, we only parse valid tokens
-	scopes := make([]string, 0, 0)
-	rawclaims, _ := token.Claims["scope"].([]interface{})
-	for _, rawclaim := range rawclaims {
-		scope, _ := rawclaim.(string)
-		scopes = append(scopes, scope)
-	}
-	scopestring = strings.Join(scopes, ",")
-	return
 }
