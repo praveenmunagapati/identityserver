863fee294b3fc5cc248aeaf6f6e003f0382e3289
Lee Smet <lee.smet@hotmail.com>
2017-02-23 11:18:14 +0100

    Fix #457: support Array.includes in IE11

--- siteservice/website/components/patches.js
+++ siteservice/website/components/patches.js
@@ -18,6 +18,47 @@ function initializePolyfills() {
           }
       };
   }
+  if (!Array.prototype.includes) {
+        Object.defineProperty(Array.prototype, 'includes', {
+            value: function(search, start) {
+
+              // First check if the array is defined
+              if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+              }
+
+              // Get the object of the array
+              var arr = Object(this);
+              // NOP zero fill right bitshift, actually just a cast to
+              // a 32bit uint
+              var length = arr.length >>> 0;
+              // If array length is 0, it contains nothing so include returns
+              // false per definition
+              if (length === 0) {
+                return false;
+              }
+              // Start searchin at the given index. Start at 0 (the beginning)
+              // if no start index is defined
+              var n = start | 0;
+
+              // searchPtr is the actual search index. If the defined start (n) is bigger than
+              // 0, use that (searchPtr = n). Else add the negative index to the length of the array
+              // if searchPtr is negative after this, set searchPtr to 0
+              var searchPtr = Math.max(n >= 0 ? n : length - Math.abs(n), 0);
+
+              // Repeat from the calculated start index untill the end
+              while (searchPtr < length) {
+                if (arr[searchPtr] === search) {
+                  return true;
+                }
+                searchPtr++;
+              }
+
+              // If we didn't find the result above, return false
+              return false;
+            }
+      });
+}
   if (!Array.prototype.find) {
       Object.defineProperty(Array.prototype, 'find', {
           value: function (predicate) {
