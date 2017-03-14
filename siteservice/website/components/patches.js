6f5d8b1aeff8092d64da2a2cc4478d58d811c707
Lucas Vanhalst <lucas@mobicage.com>
2017-03-14 09:54:17 +0100

    Fix string.startsWith polyfill for ancient browsers

--- siteservice/website/components/patches.js
+++ siteservice/website/components/patches.js
@@ -1,7 +1,8 @@
 function initializePolyfills() {
   if (!String.prototype.startsWith) {
-      String.prototype.startsWith = function (haystack, needle) {
-          return haystack.lastIndexOf(needle, 0) === 0;
+      String.prototype.startsWith = function (searchString, position) {
+          position = position || 0;
+          return this.substr(position, searchString.length) === searchString;
       };
   }
   if (!String.prototype.includes) {
