4366bf8cfd0dffe275c7b53d862858cebd09d409
Lee Smet <lee.smet@hotmail.com>
2017-05-16 09:52:10 +0200

    Fix #500: Fix javascript tests

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,8 @@ module.exports = function(config) {
           'components/shared/shared.js',
           'components/shared/configService.js',
           'components/patches.js',
+          'components/shared/country-info.js',
+          'components/shared/directives/telinput.js',
 
           // load main app and dependancies
           'components/app.js',
